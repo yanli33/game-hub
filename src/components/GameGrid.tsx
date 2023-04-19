9b0166d8185e992ca687afb6f697608cdee2036d
YanLi <2319555140@qq.com>
2023-04-19 12:06:10 +0800

    Refractor:remove duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,13 +2,11 @@ import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
+import GameCardContainer from "./GameCardContainer"
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames()
   const skeletons = [1, 2, 3, 4, 5, 6]
-  // console.log(111)
-  // console.log(games)
-  // console.log(games[0].name)
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -16,10 +14,14 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+            </GameCardContainer>
           ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
